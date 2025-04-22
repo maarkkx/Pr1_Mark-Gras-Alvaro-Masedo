56ecaca6468813cee6bd524d471cf88c310efb62
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-04-22 12:18:33 +0200

    DBConnection feta Correctament

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DBConnection.openCon(DBConnection.dbPath);
+        Connection con = DBConnection.openCon(DBConnection.dbPath);
 
-        if (conn != null) {
+        if (con != null) {
             System.out.println("Connexió amb la BD feta correctament.");
         } else {
             System.out.println("No s'ha pogut establir la connexió.");
