4461a2b32acfcd5bd4f664be6e49de5868485ddb
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-04-22 12:06:00 +0200

    DBConnection feta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import dao.DBConnection;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+        Connection conn = DBConnection.openCon(DBConnection.dbPath);
 
+        if (conn != null) {
+            System.out.println("Connexió amb la BD feta correctament.");
+        } else {
+            System.out.println("No s'ha pogut establir la connexió.");
+        }
     }
 }
--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,7 +1,8 @@
 package dao;
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 public class DBConnection {
     public static String dbPath = "jdbc:sqlite:BaseDades.db";
@@ -11,12 +12,11 @@ public class DBConnection {
      * @param url Protocol i ruta cap a la BD<br>Exemple: "jdbc:sqlite:prova.db"
      * @return Connexió amb la BD o 'null' si no s'ha pogut connectar
      */
-    private static Connection openCon(String url) {
+    public static Connection openCon(String url) {
         Connection con = null;
 
         try {
             con = DriverManager.getConnection(url);
-            System.out.println("Connexió establerta");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
