6368322ea2ff0336b67b50bdf6160b4e82d3cee9
mark gras ferrer <user.name>
2025-04-22 11:17:28 +0200

    Inteficie CRUD afegida

---
+++ src/model/CRUD.java
@@ -0,0 +1,19 @@
+package model;
+
+public interface CRUD {
+    public static void crear() {
+
+    }
+
+    public static void llegir() {
+
+    }
+
+    public static void actualitzar() {
+
+    }
+
+    public static void eliminar() {
+
+    }
+}
