a022addd766b73b5b80bcd666fb90860e43ccc95
mark gras ferrer <user.name>
2025-04-23 12:01:55 +0200

    Modificat url base de dades

--- src/dao/SQLite/SQLiteEscaladorsDAO.java
+++ src/dao/SQLite/SQLiteEscaladorsDAO.java
@@ -1,12 +1,17 @@
 package dao.SQLite;
 
+import dao.DBConnection;
 import model.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class SQLiteEscaladorsDAO implements CRUD<Escaladors> {
 
     @Override
     public void crear(Escaladors escalador) {
-
+        String sql = "INSERT INTO escaladors (id_escalador, nom, alies, edat, nivell, nom_via, estil) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        Connection con = DBConnection.openCon();
     }
 
 
