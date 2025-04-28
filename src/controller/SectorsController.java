ed8e5a405d401c143e1be0ce9c4d1e534893a443
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-04-28 08:23:14 +0200

    SectorsController

--- src/controller/SectorsController.java
+++ src/controller/SectorsController.java
@@ -1,8 +1,12 @@
 package controller;
+import java.util.Scanner;
 
 public class SectorsController {
-
+    Scanner scan = new Scanner(System.in);
     public void afegirSector(){
         System.out.println("Introdueix el nom de l'escola on es troba el sector");
+
+        String nomEscola = scan.nextLine();
+
     }
 }
